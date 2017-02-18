fa22755913f8174e942e2ec4ec75475b9d625b54
C.W.B <1141644656@qq.com>
2017-02-18 08:34:44 +0800

    update

---
+++ java/src/C_E_Exercise10.java
@@ -0,0 +1,10 @@
+// by C.W.B
+// vampire number
+
+public class C_E_Exercise10 {
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- java/src/C_E_Exercise9.java
+++ java/src/C_E_Exercise9.java
@@ -3,11 +3,18 @@
 
 public class C_E_Exercise9 {
 	public static void main(String[] args) {
-		Fibonacci_sequence(10);
+		Fibonacci_sequence(20);
+		System.out.println();
+		
+		fib_recursion(20);
+		System.out.println();
+		
+		Binet_formula(20);
+		System.out.println();
 	}
 	
 	static void Fibonacci_sequence(int num) {
-		if (num < 1) {
+		if(num < 1) {
 			System.out.println("None");
 			return;
 		}
@@ -17,11 +24,28 @@ public class C_E_Exercise9 {
 		int temp1 = 0;
 		int temp2 = 1;
 		int temp;
-		for (int i = 1; i < num; i++) {
+		for(int i = 1; i < num; i++) {
 			temp = temp1 + temp2;
 			System.out.print(temp + " ");
 			temp1 = temp2;
 			temp2 = temp;
+		}
+	}
+	
+	static int fib(int n) { 
+		if(n <= 2)
+			return 1; 
+		return fib(n-1) + fib(n-2); 
+	}
+	static void fib_recursion(int n) {
+		for(int i = 1; i <= n; i++)
+			System.out.print(fib(i) + " ");
+	}
+	
+	static void Binet_formula(int n) {
+		for(int i = 1; i <= n; i++) {
+			int fib = (int)Math.round(1 / Math.sqrt(5) * (Math.pow((1 + Math.sqrt(5)) / 2, i) - Math.pow((1 - Math.sqrt(5)) / 2, i)));
+			System.out.print(fib + " ");
 		}
 	}
 }
